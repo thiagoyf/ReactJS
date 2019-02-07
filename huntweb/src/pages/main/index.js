ef20e3fe76c6861fbc7ec064221b09369b88e755
thiagoyf <thiagoyudifukunaga@gmail.com>
2019-02-07 20:02:42 -0300

    commiting full project

--- huntweb/src/pages/main/index.js
+++ huntweb/src/pages/main/index.js
@@ -1,27 +1,64 @@
 import React, { Component } from "react";
 import api from "../../services/api";
+import { Link } from "react-router-dom";
+
+import "./styles.css";
 
 export default class Main extends Component {
   state = {
-    products: []
+    products: [],
+    productInfo: {}
   }
 
   componentDidMount() {
     this.loadProducts();
   }
 
-  loadProducts = async () => {
-    const response = await api.get('/products');
+  loadProducts = async (page = 1) => {
+    const response = await api.get(`/products?page=${page}`);
 
-    this.setState({ products: response.data.docs });
+    const { docs, ...productInfo } = response.data;
+
+    this.setState({ products: docs, productInfo });
+  }
+
+  prevPage = () => {
+    const { productInfo } = this.state;
+
+    if (parseInt(productInfo.page) === 1) return;
+
+    this.loadProducts(parseInt(productInfo.page) - 1);
+  }
+
+  nextPage = () => {
+    const { productInfo } = this.state;
+
+    if (parseInt(productInfo.page) === productInfo.pages) return;
+
+    this.loadProducts(parseInt(productInfo.page) + 1);
   }
 
   render() {
+    const { products, productInfo } = this.state;
+
     return (
       <div className="product-list">
-        {this.state.products.map(product => (
-          <h2 key={product._id}>{product.title}</h2>
+        {products.map(product => (
+          <article key={product._id}>
+            <strong>{product.title}</strong>
+            <p>{product.description}</p>
+
+            <Link to={`/products/${product._id}`}>Acessar</Link>
+          </article>
         ))}
+        <div className="actions">
+          <button disabled={parseInt(productInfo.page) === 1} onClick={this.prevPage}>
+            Anterior
+          </button>
+          <button disabled={parseInt(productInfo.page) === productInfo.pages} onClick={this.nextPage}>
+            Próxima
+          </button>
+        </div>
       </div>
     )
   }
--- huntweb/src/services/api.js
+++ huntweb/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({ 
-  baseURL: 'https://rocketseat-node.herokuapp.com/api' 
+  baseURL: 'http://localhost:3001/api' 
 });
 
 export default api;
